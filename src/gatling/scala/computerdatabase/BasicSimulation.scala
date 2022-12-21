4243c19d6f31fd5f40b5ff7af16346cee93e4687
samarpit bhatia <samarpit.bhatia@oath.com>
2022-12-21 09:51:47 -0800

    add scenario for Ad Request

--- src/gatling/scala/computerdatabase/BasicSimulation.scala
+++ src/gatling/scala/computerdatabase/BasicSimulation.scala
@@ -2,51 +2,26 @@ package computerdatabase
 
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
+
+import java.util.UUID
 import scala.concurrent.duration._
 
 class BasicSimulation extends Simulation {
 
   val httpProtocol = http
-    .baseUrl("http://computer-database.gatling.io") // Here is the root for all relative URLs
-    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8") // Here are the common headers
-    .acceptEncodingHeader("gzip, deflate")
-    .acceptLanguageHeader("en-US,en;q=0.5")
-    .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0")
+    .disableCaching
+    .baseUrl("http://volcano.aps1.o2.verizonmedia.com") // Here is the root for all relative URLs
+    .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36")
+    .header("x-forwarded-for","2607:fb90:8daf:679d:b835:94ff:fe4a:851f, 69.147.64.149,69.147.64.80")
+
+  val api = "/volcano?AdBreakId=1665665987&bcid=5b152b6e7cce6e408cbfd920&pid=5bd9ec3034f69d6f92b23e52&secure=true&rssId=c6f68d2d-d06b-3d6c-a453-f817c840ba70&v=2&f=json&s2s=true&output=vast3&mode=YXS&m.podmax=60000&m.type=live&show_name=LEM&m.spaceid=1183300001&experience=y20&gdpr=false&site=finance&licensor_id=a077000000KfSioAAF&us_privacy=1YNN&m.url=https%3A%2F%2Ffinance.yahoo.com%2Fthomas-test-nov-15-200648297.html&width=822&pl=up&region=US&lang=en-US&device=desktop&plseq=1&height=619&sid="
 
   val scn = scenario("Scenario Name") // A scenario is a chain of requests and pauses
-    .exec(http("request_1")
-      .get("/"))
-    .pause(7) // Note that Gatling has recorder real time pauses
-    .exec(http("request_2")
-      .get("/computers?f=macbook"))
-    .pause(2)
-    .exec(http("request_3")
-      .get("/computers/6"))
-    .pause(3)
-    .exec(http("request_4")
-      .get("/"))
-    .pause(2)
-    .exec(http("request_5")
-      .get("/computers?p=1"))
-    .pause(670.milliseconds)
-    .exec(http("request_6")
-      .get("/computers?p=2"))
-    .pause(629.milliseconds)
-    .exec(http("request_7")
-      .get("/computers?p=3"))
-    .pause(734.milliseconds)
-    .exec(http("request_8")
-      .get("/computers?p=4"))
-    .pause(5)
-    .exec(http("request_9")
-      .get("/computers/new"))
-    .pause(1)
-    .exec(http("request_10") // Here's an example of a POST request
-      .post("/computers")
-      .formParam("""name""", """Beautiful Computer""") // Note the triple double quotes: used in Scala for protecting a whole chain of characters (no need for backslash)
-      .formParam("""introduced""", """2012-05-30""")
-      .formParam("""discontinued""", """""")
-      .formParam("""company""", """37"""))
+    .repeat(100) {
+      exec(http("Ad Request")
+        .get(api + UUID.randomUUID().toString())
+        .check(bodyLength.gt(5000)))
+    }
 
   setUp(scn.inject(atOnceUsers(1)).protocols(httpProtocol))
 }
