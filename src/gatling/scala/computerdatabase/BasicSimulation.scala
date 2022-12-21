5586735d266edfbbb3762f5129fac3480b7016fa
samarpit bhatia <samarpit.bhatia@oath.com>
2022-12-21 11:20:45 -0800

    check if response is from envoy

--- src/gatling/scala/computerdatabase/BasicSimulation.scala
+++ src/gatling/scala/computerdatabase/BasicSimulation.scala
@@ -20,11 +20,17 @@ class BasicSimulation extends Simulation {
 
   val api = "/volcano?AdBreakId=1665665987&bcid=5b152b6e7cce6e408cbfd920&pid=5bd9ec3034f69d6f92b23e52&secure=true&rssId=c6f68d2d-d06b-3d6c-a453-f817c840ba70&v=2&f=json&s2s=true&output=vast3&mode=YXS&m.podmax=60000&m.type=live&show_name=LEM&m.spaceid=1183300001&experience=y20&gdpr=false&site=finance&licensor_id=a077000000KfSioAAF&us_privacy=1YNN&m.url=https%3A%2F%2Ffinance.yahoo.com%2Fthomas-test-nov-15-200648297.html&width=822&pl=up&region=US&lang=en-US&device=desktop&plseq=1&height=619&sid="
 
+  val maybeCheckServer = baseUrl.contains("https") match {
+    case false => header("server").is("envoy")
+    case true => header("server").isNull
+  }
+
   val scn = scenario("Scenario Name") // A scenario is a chain of requests and pauses
     .repeat(repeat.toInt) {
       exec(http("Ad Request")
         .get(api + UUID.randomUUID().toString())
-        .check(bodyLength.gt(5000)))
+        .check(bodyLength.gt(5000))
+        .check(maybeCheckServer))
     }
 
   setUp(scn.inject(atOnceUsers(connections.toInt)).protocols(httpProtocol))
